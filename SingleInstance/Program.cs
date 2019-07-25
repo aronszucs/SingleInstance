b2f8a21f8a7ba4b08bcc7f73e367a4b7fbe6ffae
aronszucs <szucsaron@hotmail.com>
2019-07-25 10:25:10 +0200

    task completed

--- SingleInstance/Program.cs
+++ SingleInstance/Program.cs
@@ -3,13 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+
 
 namespace SingleInstance
 {
     class Program
     {
+        private static string MutexName = "runmeonce";
         static void Main(string[] args)
         {
+            Mutex mutex = null;
+            while (true)
+            {
+                try
+                {
+                    mutex = Mutex.OpenExisting(MutexName);
+                    mutex.Close();
+                    Console.WriteLine("Mutex found");
+                    break;
+                }
+                
+                catch (WaitHandleCannotBeOpenedException)
+                {
+                    mutex = CreateMutex();
+                }
+                Console.ReadKey();
+            }
         }
+
+        static Mutex CreateMutex()
+        {
+            bool owned = true;
+            Console.WriteLine("Mutex not found. Creating mutex.");
+            return new Mutex(owned, MutexName);
+        }
+        
     }
 }
